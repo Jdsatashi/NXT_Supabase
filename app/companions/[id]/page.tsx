063e014cc39f263d31ca39d4f7f177531916c372
Jdsatashi <vdt1073@gmail.com>
2025-05-26 14:08:30 +0700

    First commit

---
+++ app/companions/[id]/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ConpanionSession() {
+  return <div>ConpanionSession</div>;
+}
+
+export default ConpanionSession;
---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Button } from "@/components/ui/button";
+
+const Page = () => {
+  return (
+    <>
+      <h1 className="text-2xl underline">Hello world!</h1>
+      <Button>Click me</Button>
+    </>
+  );
+};
+
+export default Page;
