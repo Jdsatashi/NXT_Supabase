b5a62a40e8e4bdab1b92763776f658e5dc46e77c
Jdsatashi <vdt1073@gmail.com>
2025-05-27 20:28:26 +0700

    Implement add companion form

--- app/companions/page.tsx
+++ app/companions/page.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function Companion() {
-  return <div>Companion Menu</div>;
+  return (
+    <div className="min-lg:w-1/3 min-md:w-2/3 items-center justify-center">
+      Companion
+    </div>
+  );
 }
 
 export default Companion;
